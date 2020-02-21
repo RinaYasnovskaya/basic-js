ed4bf898f234ec1645bc8a058b79cdb7187aea32
Karina Yasnovskaya <rina.yasnovskaya@gmail.com>
2020-02-21 14:40:19 +0400

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  //throw 'Not implemented';
   // remove line with error and write your code here
+
+  let res = [];
+  if(Array.isArray(members) && members.length > 0){
+
+  	for( let i = 0; i <members.length; i++){
+ 		 	if(typeof(members[i]) === 'string'){
+      	res.push(members[i].trim()[0]);
+      }
+      else continue;
+  	}
+    return res.map(elem => elem.toUpperCase()).sort().join('');
+  }
+  else return false;
 };
